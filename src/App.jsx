ec9586ce9d8909dc6b5ff99ec377056f8d44d3ba
Hadole Raju <67407713+rajureddyhadole@users.noreply.github.com>
2024-10-01 21:17:12 +0530

    add hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import HeroSection from './components/HeroSection'
 
 const App = () => {
   return (
     <>
       <Navbar />
+      <div className='px-6 pt-20 mx-auto max-w-7xl'>
+        <HeroSection />
+      </div>
     </>
   )
 }
